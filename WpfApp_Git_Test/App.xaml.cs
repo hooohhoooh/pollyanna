4a827daf007ea14128387c210f5ce4865d44d96b
hoohhooooo <oia.aegean@nifty.com>
2014-04-25 02:53:13 +0900

    git command tool

---
+++ WpfApp_Git_Test/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp_Git_Test
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThreadAttribute]
+        public static void Main(string[] args)
+        {
+
+            var app = new App();
+            app.InitializeComponent();
+            //app._InitializeComponent();
+
+            CloneWindow w = new CloneWindow();
+            w.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            //MainWindow w = new MainWindow();
+            //app.Startup += app_Startup;
+            app.Run(w);
+        }
+    }
+}
